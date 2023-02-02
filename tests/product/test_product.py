17ce03a8c6b582453d2697f714b70a8f864ac4ce
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-02-02 15:33:56 -0300

    test: testing Product class init

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -2,5 +2,12 @@ from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    Product
-    pass
+    arguments = {"id": 1, "nome_do_produto": "Produto",
+                 "nome_da_empresa": "Empresa",
+                 "data_de_fabricacao": "01/31/2023",
+                 "data_de_validade": "01/31/2024",
+                 "numero_de_serie": "123",
+                 "instrucoes_de_armazenamento": "Instruções"}
+    product = Product(**arguments)
+    for key in arguments:
+        assert getattr(product, key) == arguments[key]
