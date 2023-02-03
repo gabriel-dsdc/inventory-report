b397d0a206a08cbc5305cd39a32686be056e91b8
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-02-02 23:59:06 -0300

    feat: generate complete report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,18 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(self, products: list[dict]) -> str:
+        companies = {}
+        for product in products:
+            company_name = product["nome_da_empresa"]
+            companies[company_name] = companies.get(company_name, 0) + 1
+
+        output = "Produtos estocados por empresa:\n"
+        for company_name in companies:
+            output += f"- {company_name}: {companies[company_name]}\n"
+        return (
+            f"{super().generate(products)}\n"
+            f"{output}"
+        )
