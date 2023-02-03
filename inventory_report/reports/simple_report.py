f79fb447bd3f8fb20653f28035d205283a4a000f
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-02-02 22:33:41 -0300

    feat: generate report method

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,16 @@
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(self, products: list[dict]) -> str:
+        manufacturing_date_list = []
+        expiration_date_list = []
+        companies = {}
+        for product in products:
+            manufacturing_date_list.append(product["data_de_fabricacao"])
+            expiration_date_list.append(product["data_de_validade"])
+            company_name = product["nome_da_empresa"]
+            companies[company_name] = companies.get(company_name, 0) + 1
+        return (
+            f"Data de fabricação mais antiga: {min(manufacturing_date_list)}\n"
+            f"Data de validade mais próxima: {min(expiration_date_list)}\n"
+            f"Empresa com mais produtos: {max(companies, key=companies.get)}"
+        )
