3729caeed7642c21eeed941eea355fc3683e11a2
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-02-04 00:31:46 -0300

    test: product_report __repr__

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    arguments = {"id": 1, "nome_do_produto": "Produto",
+                 "nome_da_empresa": "Empresa",
+                 "data_de_fabricacao": "01/31/2023",
+                 "data_de_validade": "01/31/2024",
+                 "numero_de_serie": "123",
+                 "instrucoes_de_armazenamento": "Instruções"}
+    product = Product(**arguments)
+    assert repr(product) == (
+        f"O produto {arguments['nome_do_produto']}"
+        f" fabricado em {arguments['data_de_fabricacao']}"
+        f" por {arguments['nome_da_empresa']} com validade"
+        f" até {arguments['data_de_validade']}"
+        f" precisa ser armazenado {arguments['instrucoes_de_armazenamento']}."
+    )
