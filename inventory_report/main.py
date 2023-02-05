d606ebb7921a9fed2c9f18e117d908d7241aa3ca
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-02-04 23:45:00 -0300

    fix: main.py stderr linebreak

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -7,7 +7,7 @@ from inventory_report.inventory.inventory_refactor import InventoryRefactor
 
 def main():
     if len(sys.argv) < 3:
-        return sys.stderr.write("Verifique os argumentos")
+        return sys.stderr.write("Verifique os argumentos\n")
         # return print("Verifique os argumentos", file=sys.stderr)
     path = sys.argv[1]
     report_type = sys.argv[2]
